43ff23b808446653b8457b5113d1c408ff3bf707
Sheri Kwong <sherikwong@gmail.com>
2020-09-03 21:05:30 -0400

    Auto speaker item

--- src/components/item/create/Create.tsx
+++ src/components/item/create/Create.tsx
@@ -1,14 +1,48 @@
-import { Box } from 'grommet';
-import React from 'react';
+import { Box, TextInput } from 'grommet';
+import { Microphone } from 'grommet-icons';
 import QrCode from 'qrcode.react';
+import React, { useRef, useState } from 'react';
+import DictateButton from 'react-dictate-button';
 import uuid from 'react-uuid';
+import styled from 'styled-components';
+
+const DictationButtonWrapper = styled(DictateButton)`
+  background-color: transparent;
+  border-color: transparent;
+`;
+
 
 const Create = () => {
   const randomID = uuid();
+  const [itemName, setItemName] = useState('');
 
-  return (<Box>
-    <QrCode value={randomID}></QrCode>
-  </Box>);
+  const onDictate = res => {
+    if (res) {
+      console.log(res);
+      setItemName(res.result.transcript);
+    }
+  }
+
+  const buttonRef = useRef(null);
+  const onClick = event => {
+    console.log(event);
+    console.log(buttonRef)
+  }
+
+
+  return (
+    <Box align="center" justify="center">
+      <QrCode
+        value={randomID} />
+      <Box direction="row" align="center">
+
+        <TextInput value={itemName} />
+        <DictationButtonWrapper ref={buttonRef} id="dictation-button" onDictate={onDictate}>
+          <Microphone />
+        </DictationButtonWrapper>
+      </Box>
+    </Box>
+  );
 };
 
 export default Create;
