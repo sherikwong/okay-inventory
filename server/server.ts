f25037e5cb9e2a00bc047e13a9c21f2f0c59c5a6
Sheri Kwong <sherikwong@gmail.com>
2020-09-04 18:04:42 -0400

    Linking express and react

--- server/server.ts
+++ server/server.ts
@@ -1,17 +1,27 @@
 import { Express, Request, Response } from "express";
+import express from 'express';
+import path from 'path';
 
 export class Server {
 
   private app: Express;
 
   constructor(app: Express) {
+
+
     this.app = app;
+
+    this.app.use(express.static(path.resolve("./") + "/public"));
 
     this.app.get("/api", (req: Request, res: Response): void => {
       res.send("You have reached the API!");
-    })
-  }
+    });
 
+    this.app.get("*", (req: Request, res: Response): void => {
+      res.sendFile(path.resolve("./") + "/src/index");
+    });
+
+  }
   public start(port: number): void {
     this.app.listen(port, () => console.log(`Server listening on port ${port}!`));
   }
