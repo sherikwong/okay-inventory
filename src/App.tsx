a457361a430b44563eaabc94e30ac2e322839156
Sheri Kwong <sherikwong@gmail.com>
2020-09-08 20:38:01 -0400

    Delete files for now

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import ModalContext from './components/modal/ModalContext';
 import Modal from './components/modal/Modal';
 // import List from './components/List/List';
 import { createBrowserHistory } from 'history';
-import Item from './components/Item/Item';
+// import Item from './components/Item/Item';
 import EditItem from './components/Item/EditItem/EditItem';
 
 const theme = {
@@ -41,7 +41,7 @@ const App = () => {
 
 
                 <Route path="/item/:id/edit" component={EditItem} />
-                <Route path="/item/:id" component={Item} />
+                {/* <Route path="/item/:id" component={Item} /> */}
                 <Route exact path="/items/new" component={EditItem} />
                 {/* <Route path="/items" component={List} />
                 <Route exact path="/" component={List} /> */}
