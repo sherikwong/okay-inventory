03f739a2b8ab40550d3b203ee0ace93d89459db6
Sheri Kwong <sherikwong@gmail.com>
2020-09-14 00:33:53 -0400

    Switch port

--- server/index.tsx
+++ server/index.tsx
@@ -12,7 +12,7 @@ app.use(express.static("public"));
 app.use(express.static(path.join(__dirname, "..", "build")));
 
 // start express server on port 5000
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log("server started on port 5000");
 });
 
