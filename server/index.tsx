79a0eaef6c9734115838acb894734c1d6ec78b5f
Sheri Kwong <sherikwong@gmail.com>
2020-09-15 20:50:57 -0400

    asoif

--- server/index.tsx
+++ server/index.tsx
@@ -10,10 +10,13 @@ app.set('port', port);
 app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(express.static(path.join(__dirname, "..", "build")));
 
+app.listen(port, () => {
+  console.log("server started on port 3000");
+});
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../", "build", "index.html"));
 });
-
 
 
 module.exports = {};
