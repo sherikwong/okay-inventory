53912178f159590a5123388ccb37227e1a997345
Sheri Kwong <sherikwong@gmail.com>
2020-09-15 20:41:50 -0400

    Change order of invocations for express

--- server/index.tsx
+++ server/index.tsx
@@ -4,19 +4,19 @@ const express = require('express');
 const app = express(); // create express app
 const path = require("path");
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../", "build", "index.html"));
-});
+const port = process.env.PORT || 300;
+app.set('port', port);
 
 app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(express.static(path.join(__dirname, "..", "build")));
 
-const port = process.env.PORT || 300;
-
-// start express server on port 5000
-app.set('port', port);
 app.listen(port, () => {
   console.log("server started on port 300");
 });
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../", "build", "index.html"));
+});
+
 
 module.exports = {};
