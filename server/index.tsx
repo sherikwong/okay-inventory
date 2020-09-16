b1f6859b476aaf68f0602456ee3f207db6d4fc85
Sheri Kwong <sherikwong@gmail.com>
2020-09-15 20:48:39 -0400

    Missing 0

--- server/index.tsx
+++ server/index.tsx
@@ -4,19 +4,16 @@ const express = require('express');
 const app = express(); // create express app
 const path = require("path");
 
-const port = process.env.PORT || 300;
+const port = process.env.PORT || 3000;
 app.set('port', port);
 
 app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(express.static(path.join(__dirname, "..", "build")));
 
-app.listen(port, () => {
-  console.log("server started on port 300");
-});
-
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../", "build", "index.html"));
 });
+
 
 
 module.exports = {};
