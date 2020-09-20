fde45d41cfc255195b3315629f05ef8fcdbcc631
Sheri Kwong <sherikwong@gmail.com>
2020-09-20 17:03:06 -0400

    Built

---
+++ build/precache-manifest.61d96f036d91985210f901bdce0fc91d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "147ef55ec0b3b54b315e99d6348ec326",
+    "url": "/index.html"
+  },
+  {
+    "revision": "098ebf239981174df5a5",
+    "url": "/static/css/2.455cd81f.chunk.css"
+  },
+  {
+    "revision": "bb20d4f903a87e92b190",
+    "url": "/static/css/main.2491bbec.chunk.css"
+  },
+  {
+    "revision": "098ebf239981174df5a5",
+    "url": "/static/js/2.ef9ff9c6.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/static/js/2.ef9ff9c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb20d4f903a87e92b190",
+    "url": "/static/js/main.3e0be874.chunk.js"
+  },
+  {
+    "revision": "7e842c8f3dfe94d659c8",
+    "url": "/static/js/runtime-main.b9674246.js"
+  }
+]);
