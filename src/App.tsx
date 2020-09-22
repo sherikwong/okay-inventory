ad2637bcee1bd2362d1581e4c8d08b374bdb2800
Sheri Kwong <sherikwong@gmail.com>
2020-09-21 21:49:59 -0400

    Commenting in router

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import Modal from './components/modal/Modal';
 // import List from './components/List/List';
 import { createBrowserHistory } from 'history';
 import List from './components/List';
+import ItemRouter from './components/Item/ItemRouter';
 // import ItemRouter from './components/Item/ItemRouter';
 
 // import Item from './components/Item';
@@ -44,7 +45,7 @@ const App = () => {
                 {/* <Route path="/" component={Tags} /> */}
 
                 {/* <Route path="/item/:id/edit" component={EditItem} /> */}
-                {/* <Route path="/item/:id" component={ItemRouter} /> */}
+                <Route path="/item/:id" component={ItemRouter} />
                 {/* <Route exact path="/items/new" component={EditItem} /> */}
 
                 <Route exact path="/" component={List} />
