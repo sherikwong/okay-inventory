32fd90bf59f78d2b6eea6640a613dd0da6147de9
Sheri Kwong <sherikwong@gmail.com>
2020-09-24 17:47:51 -0400

    aosgij

--- src/components/item/Item.tsx
+++ src/components/item/Item.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useEffect, useState } from 'react';
 import { HugeArrowButtons, Header, QrCodeWrapper, DummyQRCode, ContrastingText } from './Item.styles';
-import { Box } from 'grommet';
+import { Box, Layer } from 'grommet';
 import { Down, Up } from 'grommet-icons';
 import { QrCode } from 'qrcode.react';
 import { withRouter } from 'react-router-dom';
@@ -28,33 +28,33 @@ const Item = (props) => {
 
   useEffect(() => {
     const queryDirection = queryString.parse(location.search);
-    
+
     if (queryDirection) {
-      setModal(true);  
-  alterQty(quantity + (+queryDirection));
-    } 
+      setModal(true);
+      // alterQty(quantity + (+queryDirection));
+    }
   }, []);
 
   useEffect(() => {
-setQty(details.quantity || 0);
+    setQty(details.quantity || 0);
   }, [details]);
 
- 
-  
-  return (<>
-    <Swipeable onSwipedDown={() => alterQty(-1)} onSwipedUp={() => alterQty(1)}
-  ox direction="column" fill={true} align="center" justify="between">
 
-  owButtons secondary size="large" icon={<Up />} onClick={() => alterQty(1)} />
 
-    ox align="center">
-    <Number> {quantity}</Number>
+  return (
+    <Swipeable onSwipedDown={() => alterQty(-1)} onSwipedUp={() => alterQty(1)}>
+      <Box direction="column" fill={true} align="center" justify="between">
 
-    <Header className="header-wrapper">
+        <HugeArrowButtons secondary size="large" icon={<Up />} onClick={() => alterQty(1)} />
+
+        <Box align="center">
+          <Number> {quantity}</Number>
+
+          <Header className="header-wrapper">
             {(details && details.name) ? details.name.toUpperCase() : ''}
-          < /Header>
+          </Header>
 
- 
+
           <ContrastingText> {details.date && new Date(details.date).toLocaleDateString("en-US")}
           </ContrastingText>
           {/* <CalendarIcon date={details.date} /> */}
@@ -62,16 +62,14 @@ setQty(details.quantity || 0);
           <Tags tags={details.tags} />
         </Box>
 
-<HugeArrowButtons secondary size="large" icon={<Down />} onClick={() => alterQty(-1)} />
-  
-    </Box>
+        <HugeArrowButtons secondary size="large" icon={<Down />} onClick={() => alterQty(-1)} />
 
-Swipeable>
-     
-  showModal ? (<Layer>
+      </Box>
 
-    </Layer>)
-      </>
+      {showModal && <Layer full={false}>
+<h1 className="animate__animated animate__bounce">Hello</h1>
+      </Layer>}
+    </Swipeable>
   );
 }
 
