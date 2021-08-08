33458fed7caadfca86aaf8ab729eee8c388bab6a
Sheri Kwong <sherikwong@gmail.com>
2021-08-08 16:15:46 -0400

    Remove some old code for filtering

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -42,71 +42,27 @@ interface IEditableItem extends IItem {
 }
 
 const List = ({ history }) => {
-  const [isAscQty, setAscQty] = useState(false);
-  const [isAscDate, setAscDate] = useState(false);
-  const [hasHadInitialFilter, setHasHadInitialFilter] = useState(false);
   const [filteredData, setFilteredData] = useState([] as IItem[]);
   const [reload, triggerReload] = useState(0);
 
-  const toggleSortQty = () => setAscQty(!isAscQty);
-  const toggleSortDate = () => setAscDate(!isAscDate);
+  // const toggleSortQty = () => setAscQty(!isAscQty);
+  // const toggleSortDate = () => setAscDate(!isAscDate);
 
   const items = useItems({ dependencies: reload });
 
   const [filter, setFilter] = useState({} as ListFilters | undefined);
 
-  const onFilter = (newFilter: ListFilters) => {
-    setHasHadInitialFilter(true);
-
-    if (!newFilter.name && !newFilter.tags) {
-      setFilter(undefined);
-    } else {
-      setFilter({ ...(filter ? filter : []), ...newFilter } as
-        | ListFilters
-        | undefined);
-    }
-  };
-
   useEffect(() => {
-    const filterCb = (item: IItem) => {
-      if (!!item && !!filter) {
-        const hasMatchingName =
-          item.name && filter.name
-            ? item.name.toLowerCase().includes(filter.name.toLowerCase())
-            : false;
-        // const hasMatchingTags =
-        //   filter.tags && filter.tags.size
-        //     ? !!_.intersection(item.tags, filter.tags ? [...filter.tags] : [])
-        //         .length
-        //     : true;
-        // TODO: Fix
-        const hasMatchingTags = false;
-
-        return hasMatchingName && hasMatchingTags;
-      }
-      return true;
-    };
-
-    const sortQty = (a: IItem, b: IItem) => {
-      return isAscQty ? a.quantity - b.quantity : b.quantity - a.quantity;
-    };
-    const sortDate = (a: IItem, b: IItem) => {
-      return isAscDate ? +a.date - +b.date : +b.date - +a.date;
-    };
-
     const data = Object.values(items)
-      .filter(hasHadInitialFilter ? filterCb : () => true)
       .map((item, index) => {
         if (!(item.quantity >= 0)) {
           item.quantity = 0;
         }
         return { ...item, date: new Date(item.date), index };
       });
-    // .sort(sortQty)
-    // .sort(sortDate);
 
     setFilteredData(data);
-  }, [filter, items, isAscQty, isAscDate, hasHadInitialFilter]);
+  }, [, items]);
 
   const updateDatum = (newItem: IEditableItem, triggerUpdate = true) => {
     const sanitizedItem: IEditableItem = { ...newItem };
@@ -129,106 +85,6 @@ const List = ({ history }) => {
     });
   };
 
-  let columns = [
-    {
-      property: 'selected',
-      header: <></>,
-      render: (datum) => (
-        <SelectedCell datum={datum} selectedIDs={selectedIDs} />
-      ),
-    },
-    {
-      property: 'date',
-      header: (
-        <Box direction="row" align="center">
-          <span>Date</span>
-          <SmallButton
-            icon={isAscDate ? <Up /> : <Down />}
-            onClick={toggleSortDate}
-            isSelected={true}
-          />
-        </Box>
-      ),
-      render: (datum) => (
-        <DateCell
-          datum={datum}
-          selectedIDs={selectedIDs}
-          updateDatum={updateDatum}
-        />
-      ),
-    },
-    {
-      property: 'id',
-      primary: true,
-      render: (datum) => <></>,
-      header: <></>,
-    },
-    {
-      property: 'name',
-      header: <ListNameFilter onFilter={onFilter} />,
-      render: (datum) => (
-        <NameCell
-          datum={datum}
-          selectedIDs={selectedIDs}
-          updateDatum={updateDatum}
-          history={history}
-        />
-      ),
-    },
-    {
-      property: 'tags',
-      header: <ListTagsFilter onFilter={onFilter} />,
-      render: (datum) => (
-        <TagsCell
-          datum={datum}
-          selectedIDs={selectedIDs}
-          updateDatum={updateDatum}
-        />
-      ),
-    },
-
-    {
-      property: 'quantity',
-      header: (
-        <Box direction="row" align="center">
-          <span>Qty</span>
-          <SmallButton
-            icon={isAscQty ? <Up /> : <Down />}
-            onClick={toggleSortQty}
-            isSelected={true}
-          />
-        </Box>
-      ),
-      render: (datum) => (
-        <QuantityCell
-          datum={datum}
-          selectedIDs={selectedIDs}
-          updateDatum={updateDatum}
-          selectedID={selectedIDs}
-        />
-      ),
-    },
-    {
-      property: 'actions',
-      header: <></>,
-      render: (datum) => (
-        <ActionsCell
-          datum={datum}
-          selectedIDs={selectedIDs}
-          history={history}
-          refresh={() => triggerReload(reload + 1)}
-          selectedID={selectedIDs}
-          setSelectedID={setSelectedIDs}
-        />
-      ),
-    },
-  ];
-
-  // const onSaveNew = () => {
-  //   setNewItem({} as IEditableItem);
-  //   triggerReload(reload + 1);
-  //   setSelectedIDs(new Set([]));
-  // }
 
   const [selectedIDs, setSelectedIDs] = useState(new Set([] as string[]));
 
@@ -244,39 +100,6 @@ const List = ({ history }) => {
     setSelectedIDs(_selectedIDs);
   };
 
-  // useEffect(() => {
-  //   setDataIncludingNew(newItem.id ? [newItem, ...filteredData] : filteredData);
-  // }, [filteredData, newItem]);
-
-  const goToCamera = () => {
-    history.push('/');
-  };
-
-  const [highlightIndex, setHighlightIndex] = useState(0);
-
-  const selectedStyle = {
-    background: 'rgba(255, 255, 255, .1)',
-  };
-
-  const [rowStyles, setRowStyles] = useState({});
-  const [linksToCopy, setLinksToCopy] = useState('');
-
-  const generateLinks = () => {
-    const prefix = 'http://sherikwong.com/item/';
-    return [...selectedIDs].map((id) => prefix + id).join('\n\n');
-  };
-
-  useEffect(() => {
-    const stylesAppliedToSelected = [...selectedIDs].reduce((obj, id) => {
-      return {
-        ...obj,
-        [id]: selectedStyle,
-      };
-    }, {});
-    setRowStyles(stylesAppliedToSelected);
-
-    setLinksToCopy(generateLinks());
-  }, [selectedIDs]);
 
   const deleteItems = () => {
     selectedIDs.forEach((id) => {
@@ -290,7 +113,7 @@ const List = ({ history }) => {
   };
 
 
-  const columnss = [
+  const columns = [
     {
       title: 'Date',
       dataIndex: 'date',
@@ -320,26 +143,12 @@ const List = ({ history }) => {
       <ListContainer fill={true}>
         <Router history={listHistory}>
           <FilledSwipable>
-          <Table dataSource={filteredData} columns={columnss} />;
-
-            {/* <DataTable
-              columns={columns}
-              rowProps={rowStyles}
-              data={filteredData}
-              onClickRow={onItemSelect}
-              primaryKey="id"
-              pad="xxsmall"
-            /> */}
+          <Table dataSource={filteredData} columns={columns} />;
           </FilledSwipable>
 
           <Box direction="row" justify="center" margin="medium">
             <Button icon={<Add />} onClick={addNewItem} />
 
-            {selectedIDs.size > 0 && (
-              <CopyToClipboard text={linksToCopy}>
-                <Button icon={<Copy />} />
-              </CopyToClipboard>
-            )}
 
             <Button icon={<Trash />} onClick={deleteItems} />
           </Box>
